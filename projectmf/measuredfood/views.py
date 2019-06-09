7a7f98ba299a82f7cadb131690593665d28ed468
matthias4366 <spam.matthias.h.schulz@gmail.com>
2019-06-09 14:59:51 +0200

    The navbar is done.

--- projectmf/measuredfood/views.py
+++ projectmf/measuredfood/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# # Create your views here.
-# def home(request):
-#     return render(request, 'measuredfood/home.html')
-
+# Create your views here.
 def home(request):
-    """
-    Building a minimal home view for the start.
-    """
-    return HttpResponse('<h1>Welcome to measured food.</h1>')
+    return render(request, 'measuredfood/home.html')
+
+# def home(request):
+#     """
+#     Building a minimal home view for the start.
+#     """
+#     return HttpResponse('<h1>Welcome to measured food.</h1>')
--- projectmf/projectmf/settings.py
+++ projectmf/projectmf/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'measuredfood',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'projectmf.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
