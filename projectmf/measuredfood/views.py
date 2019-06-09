9a2d80688547fb04610dea01227e1cf9057094f9
matthias4366 <spam.matthias.h.schulz@gmail.com>
2019-06-09 14:40:10 +0200

    Changed settings to postgres and created a minimal homepage.

--- projectmf/measuredfood/views.py
+++ projectmf/measuredfood/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# # Create your views here.
+# def home(request):
+#     return render(request, 'measuredfood/home.html')
+
+def home(request):
+    """
+    Building a minimal home view for the start.
+    """
+    return HttpResponse('<h1>Welcome to measured food.</h1>')
