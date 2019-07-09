fb865cb72c08d44166a215ae90d468012ed4873b
matthias4366 <spam.matthias.h.schulz@gmail.com>
2019-07-09 12:02:52 +0200

    The script make_rawingredient_dict_to_json.py is now able to create a JSON file from the Python dictionary. However, some elements are still missing: pk, author and formatting.

--- projectmf/data/make_rawingredient_dict_to_json.py
+++ projectmf/data/make_rawingredient_dict_to_json.py
@@ -1,1 +1,5 @@
 from ingredients_data import ingredient_dict_list
+
+import json
+with open('data.json', 'w', encoding='utf-8') as f:
+    json.dump(ingredient_dict_list, f, ensure_ascii=False, indent=4)
