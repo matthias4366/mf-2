c3ebaa32832f65ccfa8df70f9c96b9abb30a39ef
matthias4366 <spam.matthias.h.schulz@gmail.com>
2019-07-10 16:27:58 +0200

    Inbetween state. Currently trying to create the Models SpecificIngredient and Recipe. Fairly in the dark.

--- projectmf/measuredfood/admin.py
+++ projectmf/measuredfood/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from .models import (
     RawIngredient,
-    NutrientProfile
+    SpecificIngredient,
+    NutrientProfile,
+    Recipe
     )
 
 # Register your models here.
 admin.site.register(RawIngredient)
+admin.site.register(SpecificIngredient)
 admin.site.register(NutrientProfile)
+admin.site.register(Recipe)
--- projectmf/measuredfood/models.py
+++ projectmf/measuredfood/models.py
@@ -10,6 +10,8 @@ from .ingredient_properties import (
 MAX_DIGITS_ = 20
 DECIMAL_PLACES_ = 6
 
+from string import ascii_lowercase
+
 
 class RawIngredient(models.Model):
     """
@@ -81,4 +83,109 @@ for name in INGREDIENT_FIELDS_NUTRITION:
             blank=True,
             null=True
         )
+    )
+
+
+class Recipe(models.Model):
+
+    name = models.CharField(max_length=100)
+
+    # author = models.ForeignKey(
+    #     User,
+    #     on_delete=models.CASCADE
+    # )
+
+    def __str__(self):
+        return self.name
+
+    # def get_absolute_url(self):
+    #     return reverse('list-recipes')
+
+
+class SpecificIngredient(models.Model):
+
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
+
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    rawingredient = models.ForeignKey(
+        RawIngredient,
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True
+    )
+
+    """
+    The base amounts give starting values for the amounts for the ingrediens.
+    If the group_or_fixed value is set to fixed, the amount already represents
+    the final amount. Furthermore, if two ingredients have the same
+    fixed_or_group group assignement, they will stay in the same ratio to each
+    other. So, the base_amounts are important for the ratios of the ingredients
+    at a later point.
+    """
+    base_amount = models.DecimalField(
+        max_digits=MAX_DIGITS_,
+        decimal_places=DECIMAL_PLACES_,
+        blank=True,
+        null=True
+    )
+
+    # Units
+    GRAM = 'g'
+    MILLILITRE = 'ml'
+    PILL = 'pill'
+
+    BASE_AMOUNT_UNIT_CHOICES = [
+        (GRAM, 'gram'),
+        (MILLILITRE, 'milli litre'),
+        (PILL, 'pill'),
+    ]
+
+    base_amount_unit = models.CharField(
+        max_length = 100,
+        choices = BASE_AMOUNT_UNIT_CHOICES,
+        default = GRAM,
+    )
+
+    # Group or fixed
+    GROUP_OR_FIXED_CHOICES = [
+        ('FIXED', 'fixed'),
+        ('A', 'A'),
+        ('B', 'B'),
+        ('C', 'C'),
+        ('D', 'D'),
+        ('E', 'E'),
+        ('F', 'F'),
+        ('G', 'G'),
+        ('H', 'H'),
+        ('I', 'I'),
+        ('J', 'J'),
+        ('K', 'K'),
+        ('L', 'L'),
+        ('M', 'M'),
+        ('N', 'N'),
+        ('O', 'O'),
+        ('P', 'P'),
+        ('Q', 'Q'),
+        ('R', 'R'),
+        ('S', 'S'),
+        ('T', 'T'),
+        ('U', 'U'),
+        ('V', 'V'),
+        ('W', 'W'),
+        ('X', 'X'),
+        ('Y', 'Y'),
+        ('Z', 'Z'),
+    ]
+
+    group_or_fixed = models.CharField(
+        max_length = 100,
+        choices = GROUP_OR_FIXED_CHOICES,
+        blank=True,
+        null=True
     )
