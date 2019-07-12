da0c3ad2483b68f8571617106fc552d4fbe43c3c
matthias4366 <spam.matthias.h.schulz@gmail.com>
2019-07-12 09:57:39 +0200

    Done refactoring views. Split the views file into multiple python files.

--- projectmf/measuredfood/urls.py
+++ projectmf/measuredfood/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from . import old_views
-from .views import home
+from .views import (home,
+                    register,
+                    rawingredient,
+                    nutrientprofile
+)
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', home.home, name='home'),
-    path('register/', old_views.register, name='register'),
+    path('register/', register.register, name='register'),
     path('login/',
          auth_views.LoginView.as_view(template_name='measuredfood/login.html'),
          name='login'),
@@ -15,59 +18,59 @@ urlpatterns = [
         ), name='logout'),
     path(
         'ingredients/create/',
-        old_views.CreateRawIngredient.as_view(),
+        rawingredient.CreateRawIngredient.as_view(),
         name='create-raw-ingredient'
     ),
     path(
         'ingredients/list/',
-        old_views.ListRawIngredients.as_view(),
+        rawingredient.ListRawIngredients.as_view(),
         name='list-raw-ingredients'
     ),
     path(
         'ingredients/<int:pk>/update/',
-        old_views.UpdateRawIngredient.as_view(),
+        rawingredient.UpdateRawIngredient.as_view(),
         name='update-raw-ingredient'
     ),
     path(
         'ingredients/<int:pk>/detail/',
-        old_views.DetailRawIngredient.as_view(),
+        rawingredient.DetailRawIngredient.as_view(),
         name='detail-raw-ingredient'
     ),
     path(
         'ingredients/<int:pk>/delete/',
-        old_views.DeleteRawIngredient.as_view(),
+        rawingredient.DeleteRawIngredient.as_view(),
         name='delete-raw-ingredient'
     ),
     # Nutrient profiles
     path(
         'nutrientprofile/create/',
-        old_views.CreateNutrientProfile.as_view(),
+        nutrientprofile.CreateNutrientProfile.as_view(),
         name='create-nutrient-profile'
     ),
     path(
         'nutrientprofile/list/',
-        old_views.ListNutrientProfile.as_view(),
+        nutrientprofile.ListNutrientProfile.as_view(),
         name='list-nutrient-profiles'
     ),
     path(
         'nutrientprofile/<int:pk>/update/',
-        old_views.UpdateNutrientProfile.as_view(),
+        nutrientprofile.UpdateNutrientProfile.as_view(),
         name='update-nutrient-profile'
     ),
     path(
         'nutrientprofile/<int:pk>/detail/',
-        old_views.DetailNutrientProfile.as_view(),
+        nutrientprofile.DetailNutrientProfile.as_view(),
         name='detail-nutrient-profile'
     ),
     path(
         'nutrientprofile/<int:pk>/delete/',
-        old_views.DeleteNutrientProfile.as_view(),
+        nutrientprofile.DeleteNutrientProfile.as_view(),
         name='delete-nutrient-profile'
     ),
     # Interim recipe
     path(
         'interim_recipe/creation',
-        old_views.DeleteNutrientProfile.as_view(),
+        nutrientprofile.DeleteNutrientProfile.as_view(),
         name='interim-recipe-creation'
     ),
 ]
--- projectmf/measuredfood/views/nutrientprofile.py
+++ projectmf/measuredfood/views/nutrientprofile.py
@@ -27,48 +27,50 @@ from measuredfood.ingredient_properties import (
 )
 
 
-class CreateRawIngredient(LoginRequiredMixin, CreateView):
-    model = RawIngredient
-    fields = INGREDIENT_FIELDS_ALL
+class CreateNutrientProfile(LoginRequiredMixin, CreateView):
+    model = NutrientProfile
+    all_fields = copy.deepcopy(INGREDIENT_FIELDS_NUTRITION)
+    all_fields.insert(0, 'name')
+    fields = all_fields
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
 
-class ListRawIngredients(
+class ListNutrientProfile(
     LoginRequiredMixin,
     ListView
 ):
-    model = RawIngredient
+    model = NutrientProfile
     ordering = ['name']
 
 
-class UpdateRawIngredient(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
-    model = RawIngredient
-    fields = INGREDIENT_FIELDS_ALL
+class UpdateNutrientProfile(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = NutrientProfile
+    fields = INGREDIENT_FIELDS_NUTRITION
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
     def test_func(self):
-        raw_ingredient = self.get_object()
-        if self.request.user == raw_ingredient.author:
+        nutrient_profile_ = self.get_object()
+        if self.request.user == nutrient_profile_.author:
             return True
         return False
 
 
-class DetailRawIngredient(DetailView):
-    model = RawIngredient
+class DetailNutrientProfile(DetailView):
+    model = NutrientProfile
 
 
-class DeleteRawIngredient(DeleteView):
-    model = RawIngredient
-    success_url = reverse_lazy('list-raw-ingredients')
+class DeleteNutrientProfile(DeleteView):
+    model = NutrientProfile
+    success_url = reverse_lazy('list-nutrient-profiles')
 
     def test_func(self):
-        raw_ingredient = self.get_object()
-        if self.request.user == raw_ingredient.author:
+        nutrient_profile_ = self.get_object()
+        if self.request.user == nutrient_profile_.author:
             return True
         return False
