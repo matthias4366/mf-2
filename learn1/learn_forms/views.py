1e2d3fea592bebb50c66f39539f600e8ff2bd6f3
matthias4366 <spam.matthias.h.schulz@gmail.com>
2019-07-12 14:34:56 +0200

    Currently, the formset learning project is in its original working state. I am about to mess around with it.

--- learn1/learn_forms/views.py
+++ learn1/learn_forms/views.py
@@ -8,15 +8,18 @@ def index(request, programmer_id):
     LanguageFormset = inlineformset_factory(Programmer, Language, fields=('name',))
 
     if request.method == 'POST':
-        formset = LanguageFormset(request.POST, queryset=Language.objects.filter(programmer__id=programmer.id))
+        # formset = LanguageFormset(request.POST, queryset=Language.objects.filter(programmer__id=programmer.id))
+        formset = LanguageFormset(request.POST, instance=programmer)
         if formset.is_valid():
-            instances = formset.save(commit=False)
-            for instance in instances:
-                instance.programmer_id = programmer.id
-                instance.save()
+            formset.save()
+            # instances = formset.save(commit=False)
+            # for instance in instances:
+            #     instance.programmer_id = programmer.id
+            #     instance.save()
 
             return redirect('index', programmer_id=programmer.id)
 
-    formset = LanguageFormset(queryset=Language.objects.filter(programmer__id=programmer.id))
+    # formset = LanguageFormset(queryset=Language.objects.filter(programmer__id=programmer.id))
+    formset = LanguageFormset(instance=programmer)
 
     return render(request, 'index.html', {'formset': formset})
