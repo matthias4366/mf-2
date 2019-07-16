10dcc914158e6c04a605392878467ae035799be4
matthias4366 <spam.matthias.h.schulz@gmail.com>
2019-07-16 10:28:54 +0200

    Created formset for SpecificIngredient using inlineformset_factory. There is no submit button yet. Next, I will work on a button to submit the changes both for the SpecificIngredients and the NutrientProfile.

--- projectmf/measuredfood/views/table.py
+++ projectmf/measuredfood/views/table.py
@@ -9,36 +9,57 @@ from measuredfood.forms import (
 
 from measuredfood.models import FullDayOfEating, SpecificIngredient
 
+from django.forms import modelformset_factory, inlineformset_factory
+
+## TODO: old code, delete once new one is finished.
+# def table(request, id_fulldayofeating):
+#
+#     fulldayofeating_object = FullDayOfEating.objects.get(pk=id_fulldayofeating)
+#     specificingredient_queryset = SpecificIngredient.objects.filter(fulldayofeating__id=id_fulldayofeating)
+#
+#     # This form collects all the forms for the specific ingredients that need
+#     # to be displayed in the template.
+#     list_of_forms_specificingredient = []
+#
+#     if request.method == 'POST':
+#         # Full day of eating
+#         form_fulldayofeating = FullDayOfEatingForm(request.POST, instance=fulldayofeating_object)
+#         if form_fulldayofeating.is_valid():
+#             form_fulldayofeating.save()
+#
+#         # Specific Ingredient # TODO code this part.
+#
+#     else:
+#         # Full day of eating
+#         form_fulldayofeating = FullDayOfEatingForm(instance=fulldayofeating_object)
+#
+#         # Specific Ingredient
+#         for i in specificingredient_queryset:
+#             form_specificingredient_i = SpecificIngredientForm(instance = i)
+#             list_of_forms_specificingredient.append(form_specificingredient_i)
+#
+#     return render(
+#         request,
+#         'measuredfood/table.html',
+#         {
+#             'form_fulldayofeating': form_fulldayofeating,
+#             'list_of_forms_specificingredient': list_of_forms_specificingredient,
+#          })
+
 def table(request, id_fulldayofeating):
-
     fulldayofeating_object = FullDayOfEating.objects.get(pk=id_fulldayofeating)
-    specificingredient_queryset = SpecificIngredient.objects.filter(fulldayofeating__id=id_fulldayofeating)
-
-    # This form collects all the forms for the specific ingredients that need
-    # to be displayed in the template.
-    list_of_forms_specificingredient = []
+    SpecificIngredientFormset = inlineformset_factory(FullDayOfEating, SpecificIngredient, fields=('__all__'), extra=1)
 
     if request.method == 'POST':
-        # Full day of eating
         form_fulldayofeating = FullDayOfEatingForm(request.POST, instance=fulldayofeating_object)
-        if form_fulldayofeating.is_valid():
-            form_fulldayofeating.save()
-
-        # Specific Ingredient # TODO code this part.
-
+        formset = SpecificIngredientFormset(request.POST, instance=fulldayofeating_object)
+        if formset.is_valid():
+            formset.save()
+            return redirect('update-fulldayofeating', fulldayofeating_id=fulldayofeating_object.id)
     else:
-        # Full day of eating
         form_fulldayofeating = FullDayOfEatingForm(instance=fulldayofeating_object)
-
-        # Specific Ingredient
-        for i in specificingredient_queryset:
-            form_specificingredient_i = SpecificIngredientForm(instance = i)
-            list_of_forms_specificingredient.append(form_specificingredient_i)
-
-    return render(
-        request,
-        'measuredfood/table.html',
-        {
-            'form_fulldayofeating': form_fulldayofeating,
-            'list_of_forms_specificingredient': list_of_forms_specificingredient,
-         })
+        formset = SpecificIngredientFormset(instance=fulldayofeating_object)
+        print(formset[0].fields)
+        context = {'formset': formset,
+                   'form_fulldayofeating': form_fulldayofeating,}
+        return render(request,'measuredfood/table.html', context)
