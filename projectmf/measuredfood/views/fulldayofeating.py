b75710f434edb534585527d74f34fbbcad6aacb4
matthias4366 <spam.matthias.h.schulz@gmail.com>
2019-07-17 15:15:50 +0200

    Inbetween commit. Working on creating a queryset to access the names of the ingredients used in the full day of eating in order to display them as part of the calculation results.

--- projectmf/measuredfood/views/fulldayofeating.py
+++ projectmf/measuredfood/views/fulldayofeating.py
@@ -5,6 +5,9 @@ from measuredfood.forms import (
     SpecificIngredientForm,
     FullDayOfEatingForm,
     )
+from measuredfood.models import (
+    RawIngredient
+)
 from measuredfood.models import FullDayOfEating, SpecificIngredient
 from django.forms import modelformset_factory, inlineformset_factory
 from django.urls import reverse, reverse_lazy
@@ -101,22 +104,53 @@ class DeleteFullDayOfEating(DeleteView):
 
 
 def calculate_fulldayofeating(request, id_fulldayofeating):
-    return HttpResponse("<html><body>Calculate full day of eating.</body></html>")
+    fulldayofeating_object = FullDayOfEating.objects.get(pk=id_fulldayofeating)
+    SpecificIngredientFormset = inlineformset_factory(
+        FullDayOfEating,
+        SpecificIngredient,
+        fields=('__all__'),
+        extra=1
+        )
 
-# TODO: remove this code once you don't need it anymore. It shouldn't do
-# anything, it is example code for learning purposes.
-# code from https://youtu.be/6y5Ucj5bI0Q?t=499
-def edit_post(request, slug):
-    post = get_object_or_404(Post, slug=slug)
     if request.method == 'POST':
-        form = PostForm(request.POST, instance=post)
-        if form.is_valid():
-            post = forms.save(commit=False)
-            post.author = request.user
-            post.save()
-            return redirect('blog:list_of_post_backend')
+        form_fulldayofeating = FullDayOfEatingForm(
+            request.POST,
+            instance=fulldayofeating_object
+            )
+        formset = SpecificIngredientFormset(
+            request.POST,
+            instance=fulldayofeating_object
+            )
+        if formset.is_valid() and form_fulldayofeating.is_valid():
+            formset.save()
+            form_fulldayofeating.save()
+            return redirect(
+                'update-fulldayofeating',
+                id_fulldayofeating=fulldayofeating_object.id
+                )
     else:
-        form = PostForm(instance=post)
-    template = 'blog/backend/new_post.html'
-    context = {'form': form}
-    return render(request, template, context)
+        form_fulldayofeating = FullDayOfEatingForm(
+            instance=fulldayofeating_object
+            )
+        formset = SpecificIngredientFormset(instance=fulldayofeating_object)
+        result_mockup_string = 'Your results will be displayed here.'
+        result_mockup_ingredient_list_0 = SpecificIngredient.objects.filter(
+            fulldayofeating__id=id_fulldayofeating
+            ).values('rawingredient_id')
+        print('\n\n\n\n')
+        print(result_mockup_ingredient_list_0)
+        print('\n\n\n\n')
+        result_mockup_ingredient_list = list(
+            RawIngredient.objects.filter(
+            id=1).values('name')
+            )
+        print('\n\n\n\n')
+        print(result_mockup_ingredient_list)
+        print('\n\n\n\n')
+        context = {'formset': formset,
+                   'form_fulldayofeating': form_fulldayofeating,
+                   'id_fulldayofeating': id_fulldayofeating,
+                   'result_calculation_fulldayofeating': result_mockup_string
+                   }
+        # TODO: use reverse function instead
+        return render(request,'measuredfood/fulldayofeating_calculate.html', context)
