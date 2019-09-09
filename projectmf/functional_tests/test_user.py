3d8b07723c93244e6168a7d768490007fff319e7
matthias4366 <spam.matthias.h.schulz@gmail.com>
2019-09-09 17:04:48 +0200

    The test_user_registration in the test_user file has been simplified by the removal of explicit waits and the addition of a @wait decorator. Although I am not sure I fully understand it, it works and it does not work without the decorator.

--- projectmf/functional_tests/test_user.py
+++ projectmf/functional_tests/test_user.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 #     click_navbar_item
 from .base import FunctionalTest
 from selenium.webdriver.common.keys import Keys
-import time
 from functional_tests.base import wait
 
 import sys
@@ -13,7 +12,7 @@ sys.path.insert(0, '/projectmf/data/')
 
 class NewUserProfileTest(FunctionalTest):
 
-    # @wait
+    @wait
     def test_user_registration(self):
 
         dummy_username = 'DummyUser'
@@ -34,18 +33,14 @@ class NewUserProfileTest(FunctionalTest):
         email.send_keys(dummy_email)
         password1.send_keys(dummy_password)
         password2.send_keys(dummy_password)
-        time.sleep(3)
 
         # Simulate clicking on Sign Up
         sign_up_button = self.browser.find_element_by_id('id_button_signup')
         sign_up_button.send_keys(Keys.ENTER)
 
-        time.sleep(3)
         # Check if the dummy user object exists in the database.
         query_dummy_user = User.objects.filter(
             username=dummy_username,
-            # email=dummy_email,
-            # password=dummy_password,
         )
 
         dummy_user_exists = query_dummy_user.exists()
