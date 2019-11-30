aaa1fdf9b5391e01bd0d0d58ebda37c2bc979c89
matthias4366 <spam.matthias.h.schulz@gmail.com>
2019-11-30 20:45:12 +0100

    It was learned how to access the USDA database API both at the food search endpoint previously and now also at the food details endpoint.

--- projectmf/learn/api_request_usda_learn.py
+++ projectmf/learn/api_request_usda_learn.py
@@ -1,11 +1,21 @@
 import requests
-import pprint
 import json
+
+"""
+In this file, Sandor Clegane is learning how to make requests to the FoodData 
+central API. There are two parts: making a request to the food search 
+endpoint and making a request to the food details endpoint. 
+"""
 
 # API Key for tailoredmealplans@gmail.com
 API_KEY = 'yucJ7dnpJ85gj5bFGg0RK463TZqb9gu2Gy4vvCDd'
 
-params = {'api_key': API_KEY}
+# Making a request to the food search endpoint.
+
+params = {
+    'api_key': API_KEY,
+    "pageNumber": "2",
+}
 data = {'generalSearchInput': 'chia'}
 response = requests.post(
     r'https://api.nal.usda.gov/fdc/v1/search',
@@ -13,7 +23,32 @@ response = requests.post(
     json=data
 )
 
-with open('api_response_learn.txt', 'w') as outfile:
-    json.dump(response.json(), outfile , indent=4, sort_keys=True)
+with open('api_food_search_endpoint_response.txt', 'w') as outfile:
+    json.dump(response.json(), outfile, indent=4, sort_keys=True)
 
-pprint.pprint(response.json())
+# Making a request to the food details endpoint.
+
+# People would first search for the food that they want to include from the
+# USDA database at this website
+# https://fdc.nal.usda.gov/fdc-app.html#/?query=beans%20kidney%20raw.
+
+food_central_id_raw_kidney_beans = "173744"
+
+params = {
+    'api_key': API_KEY,
+    "FoodData Central ID": food_central_id_raw_kidney_beans,
+}
+
+url_food_details = r'https://api.nal.usda.gov/fdc/v1/'\
+       + food_central_id_raw_kidney_beans \
+       + r'?api_key='\
+       + str(API_KEY)
+
+print(url_food_details)
+
+response = requests.get(
+    url_food_details,
+)
+
+with open('api_food_details_endpoint_response.txt', 'w') as outfile:
+    json.dump(response.json(), outfile, indent=4, sort_keys=True)
