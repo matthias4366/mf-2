433e83866bd6e2ad658002a811de78bdfd37941d
matthias4366 <spam.matthias.h.schulz@gmail.com>
2020-03-30 16:13:57 +0200

    The loading of the JSON data in the ingredient_properties4.py file should work now!

--- projectmf/learn/learn_import_json/folderB/main_script_2.py
+++ projectmf/learn/learn_import_json/folderB/main_script_2.py
@@ -5,10 +5,21 @@ from pathlib import Path
 print('sys.path')
 print(sys.path)
 
+print(f"Home directory: {Path.home()}")
+print(f"Current directory: {Path.cwd()}")
+
+path_to_json_file = Path.cwd().parent.joinpath('folderA').joinpath(
+    'example_json1.json')
+str_path_to_json_file = str(path_to_json_file)
+print(f'str_path_to_json_file: {str_path_to_json_file}')
+
+with open(path_to_json_file, 'r') as fp:
+    ALL_NUTRIENTS_AND_DEFAULT_UNITS = json.load(fp)
+
 # /home/matthias/1_local_code/mf-2/projectmf/learn/learn_import_json/folderA/example_json1.json
 
-with open('/home/matthias/1_local_code/mf-2/projectmf/learn/learn_import_json/folderA/example_json1.json', 'r') as fp:
-    ALL_NUTRIENTS_AND_DEFAULT_UNITS = json.load(fp)
+# with open('/home/matthias/1_local_code/mf-2/projectmf/learn/learn_import_json/folderA/example_json1.json', 'r') as fp:
+#     ALL_NUTRIENTS_AND_DEFAULT_UNITS = json.load(fp)
 
 print('ALL_NUTRIENTS_AND_DEFAULT_UNITS')
 print(ALL_NUTRIENTS_AND_DEFAULT_UNITS)
--- projectmf/measuredfood/ingredient_properties4.py
+++ projectmf/measuredfood/ingredient_properties4.py
@@ -1,35 +1,11 @@
-import sys
-sys.path.append('..')
-sys.path.append('...')
-sys.path.append('....')
-sys.path.append('.....')
-sys.path.append('......')
-
 import json
 
-import pathlib
+from pathlib import Path
 
-path_to_data = pathlib.Path(__file__).parent.absolute().joinpath('data')
-print('path_to_data')
-print(path_to_data)
-sys.path.append(path_to_data)
-print('sys.path')
-print(sys.path)
+path_to_json_file = Path.cwd().parent.joinpath('data').joinpath(
+    'nutrient_dict_list.json')
 
-cur_path = pathlib.Path(__file__).parent.absolute()
-print(cur_path)
-file_path_ = cur_path / 'data' / 'nutrient_dict_list.json'
-print('file_path_')
-print(file_path_)
-
-print('str(file_path_)')
-print(str(file_path_))
-#
-# print('sys.path')
-# print(sys.path)
-
-with open('data.nutrient_dict_list.json', 'r') as fp:
-# with open(str(file_path_), 'r') as fp:
+with open(path_to_json_file, 'r') as fp:
     ALL_NUTRIENTS_AND_DEFAULT_UNITS = json.load(fp)
 
 INGREDIENT_FIELDS_LINKS = [
