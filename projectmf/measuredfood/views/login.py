bd7e60e40e753b22356244833ded7f938c9d4a19
matthias4366 <spam.matthias.h.schulz@gmail.com>
2020-05-04 13:59:19 +0200

    The custom login view has been finished as a rough draft. When the user logs in, they are redirected appropriately according to the 'has_completed_tutorial' value of their UserProfile.

--- projectmf/measuredfood/views/login.py
+++ projectmf/measuredfood/views/login.py
@@ -1,8 +1,42 @@
 # from django.http import request
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+from django.contrib.auth.models import User
+from measuredfood.models import UserProfile
 
 
 def login_custom_view(request):
-    context_ = {}
+    if request.method == 'POST':
+        form = AuthenticationForm(request=request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.info(request, f"You are now logged in as {username}")
+                user_object = User.objects.filter(username=username)
+                user_profile_object = UserProfile.objects.filter(
+                    user=user_object[0])
+                user_has_completed_tutorial = \
+                    getattr(user_profile_object[0], 'has_completed_tutorial')
+
+                if user_has_completed_tutorial:
+                    return redirect('list-fulldayofeating')
+                else:
+                    context_ = {}
+                    return render(
+                        request,
+                        'measuredfood/initial_tutorial_1.html',
+                        context_
+                    )
+        else:
+            # TODO: display an error message
+            pass
+    form = AuthenticationForm()
+    context_ = {"form": form}
     return render(request, 'measuredfood/login.html', context_)
 
